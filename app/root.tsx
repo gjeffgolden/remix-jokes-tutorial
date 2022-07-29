23e46c4e210ce69ec7d45aa36cdb7fb5654b292d
Jeff Golden <codebygolden@gmail.com>
2022-07-29 14:33:26 -0600

    Setting up Prisma and seeds

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,30 @@
-import { LiveReload, Outlet } from "@remix-run/react";
-// useful during development to auto-refresh browser when a change is made
+import type { LinksFunction } from "@remix-run/node";
+
+import { Links, LiveReload, Outlet } from "@remix-run/react";
+// LiveReload is useful during development to auto-refresh browser when a change is made
+
+import globalStylesUrl from "./styles/global.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalLargeStylesUrl from "./styles/global-large.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl,
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)",
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)",
+    },
+  ];
+};
 
 export default function App() {
   return (
@@ -7,6 +32,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet />
